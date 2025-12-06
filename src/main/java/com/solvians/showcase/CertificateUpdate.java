d1faa66a20f13623caa3e2d53def9800128db7be
Joe Henry <joseph1310@sky.com>
2025-12-06 13:33:11 +0000

    Create CertificateUpdate interface

--- src/main/java/com/solvians/showcase/CertificateUpdate.java
+++ src/main/java/com/solvians/showcase/CertificateUpdate.java
@@ -1,6 +1,24 @@
 package com.solvians.showcase;
 
 public class CertificateUpdate {
+    private long timestamp;
+    private String isin;
+    private double bidPrice;
+    private int bidSize;
+    private double askPrice;
+    private int askSize;
 
-    // TODO: implement me.
+    public CertificateUpdate(long timestamp, String isin, double bidPrice, int bidSize, double askPrice, int askSize) {
+        this.timestamp = timestamp;
+        this.isin = isin;
+        this.bidPrice = bidPrice;
+        this.bidSize = bidSize;
+        this.askPrice = askPrice;
+        this.askSize = askSize;
+    }
+
+    public String toString() {
+        return String.format("%d,%s,%.2f,%d,%.2f,%d",
+            timestamp, isin, bidPrice, bidSize, askPrice, askSize);
+    }
 }
