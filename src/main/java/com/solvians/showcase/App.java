bcddb2eb9abcc0a08a72d38a43fb99bb83b2d05d
Joe Henry <joseph1310@sky.com>
2025-12-06 13:31:56 +0000

    cleanup main

--- src/main/java/com/solvians/showcase/App.java
+++ src/main/java/com/solvians/showcase/App.java
@@ -4,17 +4,16 @@ package com.solvians.showcase;
  * Hello world!
  */
 public class App {
-    public App(String threads, String quotes) {
-
-    }
-
     public static void main(String[] args) {
         if (args.length >= 2) {
             int threads = Integer.parseInt(args[0]);
             int quotes = Integer.parseInt(args[1]);
 
             CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
-            certificateUpdateGenerator.generateQuotes();
+            certificateUpdateGenerator.generateQuotes().forEachOrdered(
+                certificateUpdate -> System.out.println(certificateUpdate.toString())
+            );
+            System.out.println("Generated " + quotes + " quotes using " + threads + " threads.");
         }
         throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
     }
