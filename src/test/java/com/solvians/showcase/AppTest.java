ff351cb5eed766b438d5190faedfb98bcba88a36
Joe Henry <joseph1310@sky.com>
2025-12-06 13:55:41 +0000

    additional main tests

--- src/test/java/com/solvians/showcase/AppTest.java
+++ src/test/java/com/solvians/showcase/AppTest.java
@@ -9,9 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void expectTwoIntArgs() {
-        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
-            App.main(new String[]{"xxx"});
-        });
         NumberFormatException numbers = Assertions.assertThrows(NumberFormatException.class, () -> {
             App.main(new String[]{"xxx", "zzz"});
         });
@@ -21,5 +18,32 @@ public class AppTest {
         assertEquals("For input string: \"zzz\"", numbers.getMessage());
     }
 
+    @Test
+    public void expectTwoArgs() {
+        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+            App.main(new String[]{"xxx"});
+        });
 
+        thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+            App.main(new String[]{"10"});
+        });
+        assertEquals("Expected number of threads and number of quotes. But got: [10]", thrown.getMessage());
+    }
+
+    @Test
+    public void expectPositiveInts() {
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            App.main(new String[]{"0", "10"});
+        });
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            App.main(new String[]{"10", "0"});
+        });
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            App.main(new String[]{"-1", "10"});
+        });
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            App.main(new String[]{"10", "-1"});
+        });
+        assertEquals("Number of threads and quotes must be positive integers.", thrown.getMessage());
+    }
 }
