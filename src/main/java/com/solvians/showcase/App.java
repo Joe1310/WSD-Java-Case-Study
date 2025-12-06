8dd4419814804b2038d71814604b40562081d955
Joe Henry <joseph1310@sky.com>
2025-12-06 13:46:48 +0000

    ensure threads and quotes are above 0

--- src/main/java/com/solvians/showcase/App.java
+++ src/main/java/com/solvians/showcase/App.java
@@ -9,12 +9,16 @@ public class App {
             int threads = Integer.parseInt(args[0]);
             int quotes = Integer.parseInt(args[1]);
 
+            if (threads <= 0 || quotes <= 0) {
+                throw new IllegalArgumentException("Number of threads and quotes must be positive integers.");
+            }
+
             CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
             certificateUpdateGenerator.generateQuotes().forEachOrdered(
                 certificateUpdate -> System.out.println(certificateUpdate.toString())
             );
             System.out.println("Generated " + quotes + " quotes using " + threads + " threads.");
         }
-        throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
+        throw new RuntimeException("Expected number of threads and number of quotes. But got: " + args);
     }
 }
